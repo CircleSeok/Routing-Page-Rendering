2c804155e0fae290b0707e0be2ff93cce37a4c19
Wonseok <dnjstjr970302@gmail.com>
2024-10-25 15:42:35 +0900

    Use app styling and dummy data

--- app/news/page.js
+++ app/news/page.js
@@ -1,19 +1,21 @@
 import Link from 'next/link';
-
+import { DUMMY_NEWS } from '@/dummy-news';
 export default function NewsPage() {
   return (
     <>
       <h1>NewsPage</h1>
-      <ul>
-        <li>
-          <Link href='news/news-1'>News1</Link>
-        </li>
-        <li>
-          <Link href='news/news-2'>News2</Link>
-        </li>
-        <li>
-          <Link href='news/news-3'>News3</Link>
-        </li>
+      <ul className='news-list'>
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+              />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
